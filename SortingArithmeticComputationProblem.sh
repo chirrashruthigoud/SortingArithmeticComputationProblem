2b97114fba2ffefe229d9fc7346b18b3c9506e32
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-01-26 21:16:05 +0530

    Add: Compute a % b + c

--- SortingArithmeticComputationProblem.sh
+++ SortingArithmeticComputationProblem.sh
@@ -2,5 +2,5 @@ echo "Welcome To The Sorting Arithmetic Computation Problem"
 read -p "Enter a first number: " a
 read -p "Enter b first number: " b
 read -p "Enter c first number: " c
-calculate=$(( $c + $a / $b))
-echo "calculate c + a / b:-"$calculate
+calculate=$(( $a % $b + $c))
+echo "calculate a % b + c:-"$calculate
